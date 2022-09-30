1e2a61e9a711103c8e0ed3555e6925254680c808
ian4727 <elaine_gervacio@yahoo.com>
2022-09-30 18:36:51 +0800

    Successfully integrated template files to the framework SQLite Database has been set up and classes for posts under different categories

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .models import Posts
 
 # Create your views here.
 def home(request, *args, **kwargs):
@@ -10,7 +13,24 @@ def home(request, *args, **kwargs):
     return render(request, "index.html", {})
 
 def signin(request):
-        return render(request, "signin.html", {})    
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            email = email.objects.get(email=email)
+        except:
+            messages.error(request, 'User does not exist')   
+
+        email= authenticate(request, email=email, password=password)
+
+        if email is not None:
+            login(request, email)
+            return redirect('home')
+
+    context = {} 
+    return render(request, "signin.html", context)    
 
 def signup(request):
-    return render(request, "signup.html", {})    
+    context = {}
+    return render(request, "signup.html", context)    
