c6cc42dbe56e5aa176d300bd871419dab57e2c33
ian4727 <elaine_gervacio@yahoo.com>
2022-09-30 18:46:18 +0800

    database set-up

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Posts, Message, Category
+
+admin.site.register(Posts)
+admin.site.register(Message)
+admin.site.register(Category)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,40 @@
+from unicodedata import category
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    quantity = models.SmallIntegerField(max_length=200)
+    size = models.CharField (max_length=200)
+    condition = models.CharField(max_length=200)
+    notes = models.CharField(max_length=1000)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+        
+    
+    #*Category(Dropdown: Apparels, Kitchenwares, Household tools, Ornamental items, Electronics, Reading Materials, Stationary Products, Consumables, Furniture, Others
+    #Photo
+    
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__ (self):
+        return self.title
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    posts = models.ForeignKey(Posts, on_delete=models.SET_NULL, null=True)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
+
+class Category(models.Model):   
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
