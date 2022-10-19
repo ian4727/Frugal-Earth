a4e41d7f1c380a2e736f57612592f254165910e4
ian4727 <109770133+ian4727@users.noreply.github.com>
2022-10-19 16:25:26 +0800

    Update settings.py

--- frugalEarth/settings.py
+++ frugalEarth/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qo57a=7_7#da$w#-(ug$zfmcw@on(goer(jr@@s9cjaq)pltnw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
